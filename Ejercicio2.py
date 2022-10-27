2a4d9656c0b5f7061107ea1f729e4e4e8c584cce
Dan1l0p <dalova2003@gmail.com>
2022-10-27 20:58:56 +0200

    ejer 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,24 +1,14 @@
 lista = [18, 50, 210, 80, 145, 333, 70, 30]
 
-def crear_tabla(tamanio):
-    tabla = [None]* tamanio
-    return tabla
+import sys
 
-def cantidad_elementos(tabla):
-    return len(tabla)- tabla.count(None)
+def multiplo(lista):
+    aa=[]
 
-def cantidad_elementos(tabla):
-    return sum(tamanio(lista) for lista in tabla if lista is not None)
-
-def funcion_hash(dato, tamanio_tabla):
-    return len(str(dato).strip()) % tamanio_tabla
-
-def agregar(tabla, dato):
-    posicion = funcion_hash(dato , len(tabla))
-    if (tabla[posicion] is None):
-        tabla[posicion]= dato
-    else:
-        print("se produjo una colision.")
-
-
-
+    for i in range (lista):
+        if (i < 200) and (i%10 == int):
+            aa.append(i)
+            
+        if i > 300:
+        sys.exit()
+    return aa
---
+++ Ejercicio5o.py
@@ -0,0 +1,27 @@
+
+
+def crear_tabla(tamanio):
+    tabla = [None]* tamanio
+    return tabla
+
+def cantidad_elementos(tabla):
+    return len(tabla)- tabla.count(None)
+
+def cantidad_elementos(tabla):
+    return sum(tamanio(lista) for lista in tabla if lista is not None)
+
+def funcion_hash(dato, tamanio_tabla):
+    return len(str(dato).strip()) % tamanio_tabla
+
+def agregar(tabla, dato):
+    posicion = funcion_hash(dato , len(tabla))
+    if (tabla[posicion] is None):
+        tabla[posicion]= dato
+    else:
+        print("se produjo una colision.")
+        
+
+
+
+
+
