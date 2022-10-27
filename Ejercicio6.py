30471db6a32e75f9d9c5163e92eeaaa83c72fb33
Dan1l0p <dalova2003@gmail.com>
2022-10-27 20:59:02 +0200

    ejer 6

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,7 +1,7 @@
-""Realiza el  código para calcular el determinante de una matriz cuadrada de [5 x 5], regla de Sarrus de forma recursiva y de forma iterativa""
+
 
 def determinante(matriz):
-    return (matriz[0][0]*matriz[1][1]*matriz[2][2]*matriz[3][3]*matriz[4][4]*matriz[5][5])+(matriz[1][0]*matriz[0][0]*matriz[0][0]*matriz[0][0]*matriz[0][0])+(matriz[0][0]*matriz[0][0])
+    return (((matriz[0][0]*matriz[1][1]*matriz[2][2]*matriz[3][3]*matriz[4][4])+(matriz[1][0]*matriz[2][1]*matriz[3][2]*matriz[4][3]*matriz[0][4])+(matriz[2][0]*matriz[3][1]*matriz[4][2]*matriz[0][3]*matriz[1][4])+(matriz[3][0]*matriz[4][1]*matriz[0][2]*matriz[1][3]*matriz[4][4])+(matriz[4][0]*matriz[0][1]*matriz[1][2]*matriz[2][3]*matriz[3][4])))-(((matriz[0][4]*matriz[1][3]*matriz[2][2]*matriz[3][1]*matriz[4][0])+(matriz[1][4]*matriz[2][3]*matriz[3][2]*matriz[4][1]*matriz[0][0])+(matriz[2][4]*matriz[3][3]*matriz[4][2]*matriz[0][1]*matriz[1][0])+(matriz[3][4]*matriz[4][3]*matriz[0][2]*matriz[1][1]*matriz[4][0])+(matriz[4][4]*matriz[0][3]*matriz[1][2]*matriz[2][1]*matriz[3][0])))
 
 
-matriz = []
+print("El det de la matriz 5x5 es: ",determinante([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20], [21,22,23,24,25]]))
